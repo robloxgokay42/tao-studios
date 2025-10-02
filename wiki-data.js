805483f3818bb1b18b0c92a0fd88ad47b968faf8
robloxgokay42 <robloxgokay42@gmail.com>
2025-10-02 20:31:32 +0300

    Create wiki-data.js

---
+++ wiki-data.js
@@ -0,0 +1,49 @@
+// Wiki makaleleri için ana veri kaynağı
+const WIKI_ARTICLES = [
+    {
+        id: 'wikiye-merhaba',
+        title: "Wiki'ye Merhaba 👋",
+        author: 'SilaFriztche', // GÜNCELLENDİ
+        role: 'Yönetici',
+        date: '19 Eylül 2025 21:56',
+        category: 'Duyurular',
+        content: "<p>TA Studios'a ait olan wiki / blog yazısı hoş geldin bölümü!</p>",
+        isEdited: true,
+        isPinned: true
+    },
+    {
+        id: 'jandarma-liderleri',
+        title: "TA | Jandarma Liderleri",
+        author: 'sbegarry',
+        role: 'Medya Ekibi',
+        date: '22 Eylül 2025 04:58',
+        category: 'Wiki',
+        content: `
+            <p>TA içerisinde tarih boyunca Jandarma Liderliği yapmış kişilerin bulunduğu bölüm.</p>
+            <ul>
+                <li>Enesleben</li>
+                <li>cembatu12</li>
+                <li>Gokbey39</li>
+                <li>IDwright</li>
+                <li>Realhakant256</li>
+                <li>2prododo</li>
+                <li>RonFritzche</li>
+                <li>YusufW1881</li>
+            </ul>
+        `,
+        isEdited: true,
+        isPinned: false
+    },
+    {
+        id: 'deniz-kuvvetleri-liderleri',
+        title: "TA | Deniz Kuvvetleri Liderleri",
+        author: 'sbegarry',
+        role: 'Medya Ekibi',
+        date: '22 Eylül 2025 04:52',
+        category: 'Wiki',
+        content: "<p>TA içerisinde tarih boyunca Deniz Kuvvetleri Liderliği yapmış kişilerin bulunduğu bölüm.</p>",
+        isEdited: true,
+        isPinned: false
+    }
+    // Buraya yeni makaleleri ekleyebilirsiniz
+];
