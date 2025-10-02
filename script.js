36f467f85d6463c8bb72cda15246ab8778788d40
robloxgokay42 <robloxgokay42@gmail.com>
2025-10-02 20:03:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// TAO Studios JavaScript Dosyası
+// Bu dosya ileride menü açma/kapama, istatistik çekme ve diğer etkileşimler için kullanılacaktır.
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Mobil menü açma/kapama gibi etkileşimler buraya eklenecektir.
+    
+    // Örnek: Tıklanabilir alanlara basit bir loglama ekleyebiliriz (şimdilik bu gereksizdir).
+    // Tüm linkler zaten HTML'de '#' ile tıklanabilir durumdadır.
+});
